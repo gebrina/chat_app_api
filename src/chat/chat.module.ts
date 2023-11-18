d9d9be6b5535cb6a62c6cb8d041782ad72536272
gebrina <gsonofmika@gmail.com>
2023-11-19 01:44:51 +0300

    fix can't find room entity

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,7 +3,7 @@ import { UserModule } from 'src/user/user.module';
 import { ChatGateway } from './chat.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from 'src/entities/chat.entity';
-import { Room } from './room.entity';
+import { Room } from 'src/entities/room.entity';
 
 @Module({
   imports: [UserModule, TypeOrmModule.forFeature([Chat, Room])],
