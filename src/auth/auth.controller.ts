4c8fbbdfc0434fb88d84aa4903e99db9d37f0e76
gebrina <gsonofmika@gmail.com>
2023-11-20 17:34:22 +0300

    make login route public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local.guard';
+import { PublicApi } from 'src/decorators/public-api';
 
 @Controller()
 export class AuthController {
@@ -8,6 +9,7 @@ export class AuthController {
 
   @Post('auth/login')
   @UseGuards(LocalAuthGuard)
+  @PublicApi()
   login(@Request() req) {
     return this.authService.singIn(req.user);
   }
